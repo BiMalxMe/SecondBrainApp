049c0c802d023649af0d763c7766d50c1249f32e
BiMalxMe <journeyxbimal@gmail.com>
2025-02-14 21:40:50 +0545

    BE2

--- Brainly-FE/src/App.tsx
+++ Brainly-FE/src/App.tsx
@@ -1,12 +1,17 @@
+import './index.css'
 
-import './App.css'
+import { Button } from './components/Button'
 
 function App() {
 
   return (
-    <>
-      
-    </>
+   <div>
+    <h1 className='bg-purple-300'>Hey its me bimal chalse</h1>
+    <Button variant='primary' size='lg' text='Hi its bimal'></Button>
+    <Button variant='secondary' size='md' text='Hi its bimal'></Button>
+    <Button variant='secondary' size='sm' text='Hi its bimal'></Button>
+
+   </div>
   )
 }
 
--- Brainly-FE/src/components/Button.tsx
+++ Brainly-FE/src/components/Button.tsx
@@ -1,14 +1,35 @@
+import { ReactElement } from "react";
+
 interface ButtonProps {
   variant: "primary" | "secondary";
-  size: "sm" | "ls" | "md";
+  size: "sm" | "lg" | "md";
   text: string;
-  startIcon?: any;
-  endIcon?: any;
+  startIcon?: ReactElement;
+  endIcon?: ReactElement;
   onclick: () => void;
 }
 
-export const Button = (props: ButtonProps) => {
-    return <button>
+const variantStyle = {
+  primary: "bg-purple-600 text-white",
+  secondary: "bg-purple-300 text-purple-600",
+};
 
+const sizeStyles = {
+  sm: "p-2",
+  md: "p-4",
+  lg: "p-6",
+};
+
+const defaultStyles = "rounded-md";
+
+export const Button = (props: ButtonProps) => {
+  return (
+    <button
+      className={`${variantStyle[props.variant]} ${defaultStyles} ${
+        sizeStyles[props.size]
+      }`}
+    >
+      {props.text}
     </button>
+  );
 };
